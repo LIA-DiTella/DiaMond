2932787a33c71fbbf4e43706bda8bd258b5b6ac7
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2025-05-19 15:47:14 -0300

    Update dataset_obs.py

--- src/dataset_obs.py
+++ src/dataset_obs.py
@@ -1,9 +1,27 @@
 # Dataset preview
+import yaml
 import wandb
 from adni import AdniDataset
 import os
 # dataset_path = "$(shell pwd)/data/processed/hdf5"
- 
+# CONFIG ?= $(shell pwd)/config/config.yaml
+
+config_path = os.path.join(os.path.dirname(__file__), "..", "config", "config.yaml")
+
+with open(config_path, "r") as file:
+    config = yaml.safe_load(file)
+
+wandb.init(
+    project="DiaMond",
+    entity="pardo",
+    notes="Dataset preview",
+    tags=[],
+    config=config,
+    mode="online",
+    reinit="finish_previous",
+    name="dataset_preview",
+)
+
 dataset_path = os.path.join(
     os.path.dirname(__file__), "..", "data", "processed", "hdf5"
 )
@@ -18,9 +36,7 @@ train_data = AdniDataset(
     out_class_num=3,
     with_mri=wandb.config.with_mri,
     with_pet=wandb.config.with_pet,
-    allow_incomplete_pairs=wandb.config.get(
-        "allow_incomplete_pairs", False
-    ),
+    allow_incomplete_pairs=wandb.config.get("allow_incomplete_pairs", False),
 )
 
 print("train_data length: ", len(train_data))
