3e893507c4aa017e0860e803eb971267fa089fbc
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2025-05-19 16:51:39 -0300

    Update dataset_obs.py

--- src/dataset_obs.py
+++ src/dataset_obs.py
@@ -11,7 +11,6 @@ config_path = os.path.join(os.path.dirname(__file__), "..", "config", "config.ya
 with open(config_path, "r") as file:
     config = yaml.safe_load(file)
 
-
 def observe_dataset(dataset_file_path:str):
 
     wandb.init(
@@ -68,7 +67,6 @@ if __name__ == "__main__":
         print(f"  Train-Test intersection: {len(test_intersection)}")
         print(f"  Val-Test intersection: {len(val_test_intersection)}")
         all_train_rids.append(split_train_rids)
-    
 
     for i, rids in enumerate(all_train_rids):
         # check set intersection
@@ -83,3 +81,25 @@ if __name__ == "__main__":
     print(f"Total unique rids: {len(unique_rids)}")
     print(f"Total rids: {len(flattened_rids)}")
     print(f"Total rids: {len(set(flattened_rids))}")
+
+
+    original_sources = ["/home/ipardo/ADNI_data/DiaMond%20Set/ADNI", "/home/ipardo/ADNI_data/DiaMond%20Set1/ADNI", "/home/ipardo/ADNI_data/DiaMond%20Set2/ADNI", "/home/ipardo/ADNI_data/DiaMond%20Set4/ADNI", "/home/ipardo/ADNI_data/DiaMond%20Set5/ADNI", "/home/ipardo/ADNI_data/DiaMond%20Set6/ADNI", "storage1/DiaMond/tmp/DiaMond%20Set7/ADNI", "storage2/DiaMond_tmp/DiaMond%20Set3/ADNI"]
+    symlinked_sources = "storage1/DiaMond/src/data/data/ADNI Data"
+
+    all_subdirs = []
+    for source in original_sources:
+        # Check how many subdirectories are in the source directory
+        subdirs = [d for d in os.listdir(source) if os.path.isdir(os.path.join(source, d))]
+        all_subdirs.extend(subdirs)
+
+    # Check how many subdirectories are in the symlinked source directory
+    symlinked_subdirs = [d for d in os.listdir(symlinked_sources) if os.path.isdir(os.path.join(symlinked_sources, d))]
+
+    # Check if the number of subdirectories is the same
+    print(f"Original sources: {len(all_subdirs)}")
+    print(f"Symlinked sources: {len(symlinked_subdirs)}")
+
+    # Intersection of subdirectories
+    intersection = set(all_subdirs).intersection(set(symlinked_subdirs))
+    print(f"Intersection: {len(intersection)}")
+    
